b6c26906abbb05d2ef453b1a0a17b1b02e26cd89
CcccFz <ccccfz@163.com>
2023-03-05 16:21:01 +0800

    init

---
+++ solidity-start/01_hello/hello.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.4;
+
+contract HelloWeb3 {
+    string public _s = "Hello Web3333";
+    string public ss = "11";
+    string public s1 = "12";
+    constructor() {
+        
+    }
+}
